e89c23e44fdb6ed84d23380b995e3ee44db3beeb
alx-yng <alexyoung0119@gmail.com>
2024-03-09 17:51:09 -0600

    refactor js into ts

--- src/firebase/firestore/addData.ts
+++ src/firebase/firestore/addData.ts
@@ -2,7 +2,11 @@ import firebase_app from "../config";
 import { getFirestore, doc, setDoc } from "firebase/firestore";
 
 const db = getFirestore(firebase_app);
-export default async function addData(colllection, id, data) {
+export default async function addData(
+  colllection: string,
+  id: string,
+  data: any
+) {
   let result = null;
   let error = null;
 
